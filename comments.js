c04162455656b2f2fc43f268537ef4ac52f26248
lchensc <131454565+lchensc@users.noreply.github.com>
2023-12-19 10:27:17 +0000

    bugfix

--- comments.js
+++ comments.js
@@ -16,12 +16,3 @@ const { error } = require('../util/error');
 // Import validation
 const { validateComment } = require('../util/validation');
 
-// Create comment
-router.post('/', auth, async (req, res) => {
-    // Validate input
-    const { error: validationError } = validateComment(req.body);
-    if (validationError) return res.status(400).send(error(validationError.details[0].message));
-
-    // Create comment
-    const comment = new Comment({
-        ...req.body,
